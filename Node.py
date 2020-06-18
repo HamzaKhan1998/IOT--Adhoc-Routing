117786997dba6368f383b671a3ce0a57c722f9e6
HamzaKhan1998 <36290084+HamzaKhan1998@users.noreply.github.com>
2020-06-18 07:09:38 +0500

    Update and rename node.py to Node.py

--- Node.py
+++ Node.py
@@ -1,6 +1,12 @@
-import socket, pickle, thread, time, random 
+import socket
+import pickle
+import threading
+import  time
+import random
 import sys
 import copy 
+
+# print elements from specific Index till the end use [Index:]
 
 class packet:
     def __init__(self, s , d):
@@ -27,26 +33,24 @@ def listening_node(server_port):
     global req
     global rep
     s = socket.socket()          
-    print ("Socket successfully created")
+    print ("Socket Successfully Created!")
     s.bind(('', server_port))         
     print ("socket binded to %s" %(server_port)) 
     s.listen(5) 
-    print ("socket is listening")   
+    print ("Socket is Listening!")   
     
     while True: 
         # Establish connection with client. 
-        conn, addr = s.accept()      
-        #print ('Got connection from', addr) 
-        # send a thank you message to the client.  
+
+        conn, addr = s.accept() 
         data = conn.recv(4096)
         stringdata = data.decode('utf-8')
         stringdata = str(stringdata)
-        #print (stringdata)
         if (stringdata[:9]=="DSRPACKET"):
-            data_variable = pickle.loads(stringdata[10:])
+            data_variable = pickle.loads(stringdata[10:]) #Using Pickle to store and load objects
             if (len(data_variable.reply)<1):
                 print ("%.20f" % time.time())
-                print ("DSRPACKET received!!")
+                print ("DSRPACKET Received!!")
                 req=False
                 rep=False
             else:
@@ -54,8 +58,6 @@ def listening_node(server_port):
                     firstnode=data_variable.reply[-1]                
                     msg_socket = socket.socket() 
                     msg_socket.connect(('127.0.0.1', firstnode))
-                    #print ("check2",data_variable.reply)
-                    #cache[data_variable.reply[0]]=data_variable.reply
                     del data_variable.reply[-1]
                     data_string = pickle.dumps(data_variable)
                     msg_socket.send("DSRPACKET:"+data_string)
@@ -64,27 +66,22 @@ def listening_node(server_port):
                     print ("!!Drop Message!!")
             del data_variable
         elif (stringdata[:6]=="PACKET"):
-            data_variable = pickle.loads(stringdata[7:])
-            #data_variable.path = list(set(data_variable.path)) 
+            data_variable = pickle.loads(stringdata[7:])#For characters afer PACKET
             data_variable.path = [i for n, i in enumerate(data_variable.path) if i not in data_variable.path[:n]] 
             if (data_variable.dest==server_port):
                 print ("%.20f" % time.time())
-                print ("Packet received!!")
+                print ("Packet Received!!")
                 print (data_variable.path)
             else:
                 if (droprate(drop_rate)!=True):
                     for i in range(len(neighbours)):
-                        #print (neighbours,len(neighbours))
-                        #print (data_variable.path)
                         if (neighbours[i] in data_variable.path):
-                            print ("visited neighbour")
+                            print ("Visited Neighbour")
                         else:
                             msg_socket = socket.socket() 
                             msg_socket.connect(('127.0.0.1', neighbours[i]))
-                            #print ("check-x")
                             data_variable.path.append(server_port)
                             data_string = pickle.dumps(data_variable)
-                            #print ("helllo\n",data_string)
                             msg_socket.send("PACKET:"+data_string)
                             msg_socket.close()
                     
@@ -95,7 +92,7 @@ def listening_node(server_port):
             data_variable = pickle.loads(stringdata[5:])
             data_variable.path = [i for n, i in enumerate(data_variable.path) if i not in data_variable.path[:n]] 
             if (data_variable.dest==server_port):
-                print ("RREQ received!!")
+                print ("RREQ Received!!")
                 print (data_variable.path)
                 if (req==False):
                     req=True
@@ -124,15 +121,12 @@ def listening_node(server_port):
         elif (stringdata[:4]=="RREP"):
             data_variable = pickle.loads(stringdata[5:])
             if (len(data_variable.path)<1):
-                print ("RREP received!!")
-                #print (data_variable.reply)
+                print ("RREP Received!!")
                 firstnode=data_variable.reply[-1]
                 msg_socket = socket.socket() 
                 msg_socket.connect(('127.0.0.1', firstnode))
-                #cache[data_variable.reply[0]]=data_variable.reply
                 cache[data_variable.reply[0]]=copy.deepcopy(data_variable.reply)
-                #print ("check1",data_variable.reply)
-                del data_variable.reply[-1]
+                del data_variable.reply[-1] #Deleting the Last Node as we have reached our starting node
                 data_string = pickle.dumps(data_variable)
                 msg_socket.send("DSRPACKET:"+data_string)
                 msg_socket.close()
@@ -141,49 +135,45 @@ def listening_node(server_port):
                 msg_socket = socket.socket() 
                 msg_socket.connect(('127.0.0.1', firstnode))
                 cache[data_variable.reply[0]]=copy.deepcopy(data_variable.reply)
-                del data_variable.path[-1]
+                del data_variable.path[-1]#Deleting the Last Node as we traverse back further
                 data_variable.reply.append(server_port)
                 data_string = pickle.dumps(data_variable)
-                #print ("helllo\n",data_string)
                 msg_socket.send("RREP:"+data_string)
                 msg_socket.close()
             del data_variable
         elif (stringdata[:4]=="DROP"):
-            print ("Drop function")
-            #print(int(stringdata[5:]))
+            print ("Drop Function!")
             if (len(neighbours)>1):
                 neighbours.remove(int(stringdata[5:]))
                 conn.send("DONE")
             else:
-                print ("Can't break connections only 1 left")
+                print ("Can't break connections only 1 left!")
                 conn.send("ERR")
 
         else:
             neighbours.append(int(stringdata))
-            print ("Adding neighbour",neighbours)
+            print ("Adding Neighbour: ",neighbours)
 
         conn.close()      
 
 def main(filepath):
-    global drop_rate
-    #server_port =  input("Enter server port: ")            
+    global drop_rate          
     with open(filepath) as fp:
         line = fp.readline()
         cnt = 1
-        while line:
-            #print("Line {}: {}".format(cnt, line.strip()))
-            x=line.strip()
+        while line: #loop will skip the first time,increment at the end "cnt"
+            x=line.strip() #removing leading and trailing whitespaces
             if (x=="sleep"):
                 time.sleep(5)
             else:
                 if (cnt>1):
                     #print (x[0],x[2:])
                     print(x)
-                    main_input=int(x[0])
+                    main_input=int(x[0]) #checking operation number like 1,2,3...
                     if (len(x)>2):
-                        second_input=int(x[2:])
+                        second_input=int(x[2:]) #port number of neighbours
+                    
                     if (main_input == 1):
-                        #val = input("Enter port to connect to: ")
                         val=second_input
                         client_socket = socket.socket()  
                         port = val
@@ -191,31 +181,25 @@ def main(filepath):
                         neighbours.append(port) 
                         client_socket.send(str(server_port))
                         client_socket.close()
-                    elif (main_input == 2):
-                        #print ("helooo jeee")
+
+                    elif (main_input == 2): #Breaking Connection
                         if (len(neighbours)>1):
-                            rand = random.randrange(0,len(neighbours))
-                            #rand = 
-                            #print ("Random Number ", rand)
-                            #print (neighbours[rand])
+                            rand = random.randrange(0,len(neighbours)) #Generating Random Number to drop Random Neighbour
                             client_socket = socket.socket() 
                             client_socket.connect(('127.0.0.1', neighbours[rand]))
                             client_socket.send("DROP:"+str(server_port))
                             data1 = client_socket.recv(4096)
                             stringdata1 = data1.decode('utf-8')
                             stringdata1 = str(stringdata1)
-                            #print("Reply from broked node!",stringdata1)
                             if (stringdata1 == "DONE"):
                                 neighbours.pop(rand)
                                 print ("!!Dropped neighbour!!")
                             client_socket.close()
                         else:
-                            print ("Can't break connections only 1 left")
-                    elif (main_input == 3):
-                        print ("My neighbours",neighbours)
-                    elif (main_input == 4):
-                        #dp=input("Enter Destination port: ")
-                        #print("helloooo",second_input)
+                            print ("Can't break connections only 1 left!")
+                    elif (main_input == 3): #Printing Neighbours
+                        print ("My Neighbours:",neighbours)
+                    elif (main_input == 4): #Flooding
                         print ("%.20f" % time.time())
                         dp=second_input
                         p=packet(server_port,dp)
@@ -226,8 +210,7 @@ def main(filepath):
                             data_string = pickle.dumps(p)
                             client_socket.send("PACKET:"+data_string)
                             client_socket.close()
-                    elif (main_input == 5):
-                        #dp=input("Enter Destination port: ")
+                    elif (main_input == 5): #Dynamic Source Routing
                         print ("%.20f" % time.time())
                         dp=second_input
                         p=packet(server_port,dp)
@@ -236,7 +219,7 @@ def main(filepath):
                             firstnode=p.reply[-1]
                             msg_socket = socket.socket() 
                             msg_socket.connect(('127.0.0.1', firstnode))
-                            del p.reply[-1]
+                            del p.reply[-1] 
                             data_string = pickle.dumps(p)
                             msg_socket.send("DSRPACKET:"+data_string)
                             msg_socket.close()
@@ -251,14 +234,13 @@ def main(filepath):
                     elif (main_input == 6):
                         print ("Cache",cache)
                     elif (main_input == 7):
-                        print ("Setting drop rate",drop_rate)
-                        #k=input("Enter drop rate in percentage (%): ")
+                        print ("Setting Drop Rate: ",drop_rate)
                         k=second_input
                         if (k<100 and k>0):
                             drop_rate=k
                         else:
-                            print ("Out of range input!")
-                        print ("Setting drop rate",drop_rate)
+                            print ("Out of Range Input!")
+                        print ("Setting Drop Rate: ",drop_rate)
                     main_input=""
                 elif (cnt==1):
                     server_port = int(x)
